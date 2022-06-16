f7fe418f3b57700ec504622750694d135837b0d5
TimurMukhitdinov <timur.mukhitdinov@gmail.com>
2022-06-16 20:51:32 +0500

    first commit

---
+++ tests/settings.py
@@ -0,0 +1,24 @@
+# данные для позитивных тестов
+
+# данные авторизации
+valid_mail = "timur.mukhitdinov@gmail.com"
+valid_pass = "tm9323630"
+
+# данные для создания нового питомца
+valid_name = 'Djo'
+valid_animal_type = 'Khach'
+valid_age = 37
+
+# данные для модификации ранее созданного питомца
+new_valid_name = 'New Khach'
+new_valid_animal_type = 'Djordj'
+new_valid_age = 22
+
+# данные для негативных тестов
+
+# данные невалидной авторизации
+invalid_mail = '123@mail.ru'
+invalid_pass = 'jbgrqigqiuehg'
+
+# устаревший токен авторизации
+invalid_token = {'key': '2e829f586051d50a50ba66e0cf985726a959702139c2000b74de0a1e'}
